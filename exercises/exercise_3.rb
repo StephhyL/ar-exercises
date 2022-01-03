c3a209937fdac11a06938390ad8e3af9ddd36d90
StephhyL <Stephanie Li>
2022-01-03 00:49:13 -0500

    delete the third store

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -2,7 +2,17 @@ require_relative '../setup'
 require_relative './exercise_1'
 require_relative './exercise_2'
 
-puts "Exercise 3"
-puts "----------"
+puts 'Exercise 3'
+puts '----------'
 
 # Your code goes here ...
+# 1. Load the third store (into @store3) as you did the other two before
+@store3 = Store.find_by(id: 3)
+puts '----------'
+
+#Using AR's destroy method, delete the store from the database
+@store3.destroy
+puts '----------'
+#Verify that the store has been deleted by again outputting (puts ing) the count (as you did in ex. 1)
+
+puts Store.count
