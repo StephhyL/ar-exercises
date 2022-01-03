26fb8387a267dbf587eb715d4760c00bb67221b4
StephhyL <Stephanie Li>
2022-01-03 10:41:34 -0500

    update must_carry_mens_and_or_womens_apparel method as per Ruby convention

--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,25 @@
 class Store < ActiveRecord::Base
   has_many :employees
+
+  #store must have always a name that is min 3 char
+  validates :name, length: { minimum: 3, too_short: 'name too short' }
+
+  #annual_revenure => 0
+  validates :annual_revenue,
+            numericality: {
+              only_interger: true,
+              greater_than_or_equal_to: 0,
+            }
+
+  validate :must_carry_mens_and_or_womens_apparel
+
+  def must_carry_mens_and_or_womens_apparel
+    unless mens_apparel || womens_apparel
+      errors.add(:mens_apparel, 'must carry mens_apparel and/or womens_apparel')
+      errors.add(
+        :womens_apparel,
+        'must carry mens_apparel and/or womens_apparel',
+      )
+    end
+  end
 end
