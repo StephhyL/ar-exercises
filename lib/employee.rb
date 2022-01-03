1a3483a644d9ed8b4cbc10f693eb5313cf8b0fab
StephhyL <Stephanie Li>
2022-01-03 10:41:58 -0500

    code for validate hourly wage

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,12 @@
 class Employee < ActiveRecord::Base
   belongs_to :store
+
+  # first_name and last_name not null
+  validates :first_name, :last_name, presence: true
+
+  # hourly wage between 40 and 200
+  validates :hourly_rate, numericality: { in: 40...200 }
+
+  # employee must always have a store
+  validates_associated :store
 end
